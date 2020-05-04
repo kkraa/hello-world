933469a7a78e0aa978f71a6803679a49cfea4004
kkraa <52752039+kkraa@users.noreply.github.com>
2020-05-04 16:54:33 +0100

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -3,39 +3,39 @@ endpoint=$ENDPOINT
 username=$CONTROLM_CREDS_USR
 password=$CONTROLM_CREDS_PSW
 
-echo "{\"username\":\"$username\",\"password\":\"$password\"}"   >> log.txt
+echo "{\"username\":\"$username\",\"password\":\"$password\"}"   
 
 # Login
-echo "Logging in" >> log.txt
-login=$(curl -k -s -H "Content-Type: application/json" -X POST -d "{\"username\":\"$username\",\"password\":\"$password\"}" "$endpoint/session/login" )  >> log.txt
+echo "Logging in"
+login=$(curl -k -s -H "Content-Type: application/json" -X POST -d "{\"username\":\"$username\",\"password\":\"$password\"}" "$endpoint/session/login" ) 
 if [[ $login == *error* ]]; then
     echo 'Login failed!'
     exit 1
 fi
-token=$(echo ${login##*token\" : \"} | cut -d '"' -f 1)  >> log.txt
+token=$(echo ${login##*token\" : \"} | cut -d '"' -f 1) 
 
 # Test - run the jobs
 echo "Running test jobs"
-curl -k -s -H "Authorization: Bearer $token" -X POST -F "jobDefinitionsFile=@testjobs1.json" "$endpoint/deploy"  >> log.txt
-submit=$(curl -k -s -H "Authorization: Bearer $token" -X POST -F "jobDefinitionsFile=@testjobs1.json" "$endpoint/run")  >> log.txt
-runid=$(echo ${submit##*runId\" : \"} | cut -d '"' -f 1)  >> log.txt
+curl -k -s -H "Authorization: Bearer $token" -X POST -F "jobDefinitionsFile=@testjobs1.json" "$endpoint/deploy" 
+submit=$(curl -k -s -H "Authorization: Bearer $token" -X POST -F "jobDefinitionsFile=@testjobs1.json" "$endpoint/run") 
+runid=$(echo ${submit##*runId\" : \"} | cut -d '"' -f 1) 
 
 # Check job status
-jobstatus=$(curl -k -s -H "Authorization: Bearer $token" "$endpoint/run/status/$runid")  >> log.txt
-status=$(echo ${jobstatus##*status\" : \"} | cut -d '"' -f 1)  >> log.txt
+jobstatus=$(curl -k -s -H "Authorization: Bearer $token" "$endpoint/run/status/$runid") 
+status=$(echo ${jobstatus##*status\" : \"} | cut -d '"' -f 1) 
 
 echo "Waiting for jobs to end"
 # Wait until jobs have finished running
 until [[ $status == Ended* ]]; do
     sleep 10
-    tmp=$(curl -k -s -H "Authorization: Bearer $token" "$endpoint/run/status/$runid")   >> log.txt
-    echo $tmp | grep 'Not OK' >/dev/null && exit 2   >> log.txt
-    tmp2=$(echo ${tmp##*$'\"type\" : \"Folder\",\\n'})   >> log.txt
-    status=$(echo ${tmp2##*\"status\" : \"} | cut -d '"' -f 1)   >> log.txt
+    tmp=$(curl -k -s -H "Authorization: Bearer $token" "$endpoint/run/status/$runid")  
+    echo $tmp | grep 'Not OK' >/dev/null && exit 2  
+    tmp2=$(echo ${tmp##*$'\"type\" : \"Folder\",\\n'})  
+    status=$(echo ${tmp2##*\"status\" : \"} | cut -d '"' -f 1)  
 done
 
 # Logout
-curl -k -s -H "Authorization: Bearer $token" -X POST "$endpoint/session/logout"   >> log.txt
+curl -k -s -H "Authorization: Bearer $token" -X POST "$endpoint/session/logout"  
 
 # Exit
 if [[ $status == *Not* ]]; then
